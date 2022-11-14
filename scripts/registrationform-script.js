92d0a5f20c3b68616736a9857c7bd9ffb8780c60
TereseBo~ <terese.boderus@yh.nackademin.se>
2022-11-14 15:57:44 +0100

    fileregistration complete

---
+++ scripts/registrationform-script.js
@@ -0,0 +1,28 @@
+
+function projectRegistration() {//Collects form data and file names
+
+    let currentData=collectFormData("#projectregistration");//Creates object from form data
+    currentData["filenames"]=collectFileNames();//Adds filenames to object
+    let currentObjString = JSON.stringify(currentData);//Converts object to JSON-string
+    localStorage.setItem(currentData.projectname, currentObjString);//Saves JSON-string to local storage
+     //TODO: Learn how to export to server.
+    alert("Thank you! Your project has been registered.");
+    window.location.href = "index.html";
+}
+function collectFormData(form) {//Collects form data abd returns an object with label-id as key and label-value as value
+    const formData= $(form).serializeArray();
+    let newObject = {};
+    formData.forEach((item)=>{
+        newObject[item.name] = item.value;
+    })
+    return newObject;
+}
+function collectFileNames(){//Extracts file-name from string by splitting at \ and then saving last item to array
+    let fileNames = [];
+    $("input").filter("[type=file]").each(function(){
+        let temp=[]
+        temp = $(this).val().split("\\");
+        fileNames.push(temp[temp.length-1]);
+    })
+    return fileNames;
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,57 @@
-const fs=require('fs');
 
+const birdie ={
+    "projectname": "Birdie",
+    "date": "2022-02-08",
+    "description": "This plush toy with several tactile details is sure to become a favorite companion.",
+    "category": "Plush toy",
+    "warp": "Merinowool 100%",
+    "weft": "Merino wool 100%",
+    "details": "Long braided fringe,  embroidered details",
+    "filenames": [
+        "IMG_6830.JPG",
+        "IMG_6835.JPG",
+        "IMG_6835.JPG",
+        "IMG_6834.JPG"
+    ]
+}
 
+function createGalleryItem(item) {//TODO: Add all steps to create a gallery item
+}
+
+$(document).ready(function(){//TODO: Add all steps to create the gallery
+    console.log("ready");
     
-     function projectRegistration(){
-        console.log($("#projectname").val());
-        alert("Thank you! Your project has been registered.");
-        window.location.href = "index.html";
-
-     }
-     
-/*
-form.addEventListener('submit', event => {
-    // submit event detected, prevent default action and do js stuff instead
-    event.preventDefault();
-    setUserPreferences();
-    window.location.assign("draft.html");
-
-        $("#projectregistration").submit(function(event){
-        event.preventDefault();
-        $('#img1').change(function(){
-			var file = this.files[0];
-            console.log(file.name)
-		})
-
-    alert("Thank you! Your project has been registered.");
-    window.location.href = "index.html";
-    return false;
-
-     } );  
+    createCards(10);
     
-})
-*/
+    //createCards(3)
+
+
+   
+});
+
+function createCards(nr){//Creates card with class "card" and id "cardn"
+    for(let n=0; n<nr; n++){
+    $(`main`).append(`<div class='card' id='card${n}'></div>`);
+    $(`#card${n}`).text("Hello"+n);
+    $(`#card${n}`).append(`<h2></h2>`);
+    $(`#card${n}`).append(`<p></p>`);
+    $(`#card${n}`).append(`<p></p>`);
+    $(`#card${n}`).append(`<p></p>`);
+    $(`#card${n}`).append(`<p></p>`);
+    $(`#card${n}`).append(`<p></p>`);
+    $(`#card${n}`).append(`<p></p>`);
+    $(`#card${n}`).append(`<a></a>`);
+    }
+
+}
+
+function tester(current,n) {
+    let thisCard=$(`#card${n}`)
+    function placeCardContents(card,el,content){
+        card.find(el).text(content)
+    }
+    placeCardContents(thisCard,"h2",current.projectname)
+    placeCardContents(thisCard,"p",current.date)
+    placeCardContents(thisCard,"p",current.description)
+}
+
