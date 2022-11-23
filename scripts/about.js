d87a461609faf58f2077fc1b55468e8d7662db49
TereseBo~ <terese.boderus@yh.nackademin.se>
2022-11-23 12:00:24 +0100

    css update

--- scripts/about.js
+++ scripts/about.js
@@ -6,7 +6,7 @@ $(document).ready(function () {//Runs when the page is loaded
       event.preventDefault();
         console.log("clicked");
         $("#contact-form-container").empty();
-        $("#contact-form-container").append(`<p id="thankyou-message">Thank you for your message!</p>`);
+        $("#contact-form-container").append(`<p id="thankyou-message">Thank you for your message! <br><br><small>We will get back to you as soon as possible.<small></p>`);
         
     });
 
