673e7d592ae2a737711ed4a7f53bda5ee40d7428
TereseBo~ <terese.boderus@yh.nackademin.se>
2023-03-20 13:32:43 +0100

    redid links in about

--- scripts/about.js
+++ scripts/about.js
@@ -3,7 +3,6 @@
 $(document).ready(function () {//Runs when the page is loaded
     $("#contact-form-submit-button").on("click", function () {
       event.preventDefault();
-        console.log("clicked");
         $("#contact-form-container").empty();
         $("#contact-form-container").append(`<p id="thankyou-message">Thank you for your message! <br><br><small>We will get back to you as soon as possible.<small></p>`);
         
